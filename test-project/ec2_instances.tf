13f8f17e023fb8d582f22483e9a2527135db3dc1
lawrenceodedina <lawrenceodedina@gmail.com>
2025-07-01 22:35:48 -0500

    seperated provider

--- test-project/ec2_instances.tf
+++ test-project/ec2_instances.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.47.0"
-    }
-  }
-}
-
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-
 module "vpc" {
     source = "./modules/vpc"
 }
